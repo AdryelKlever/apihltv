b4bf067098372ac156908891d83352f678cb6889
Adryel Klever Vieira de Almeida <adryel.klever@callidustecnologia.com.br>
2021-10-07 11:15:40 -0300

    Ajuste nas rotas usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usuario');
 
+router.get('/', controller.get);
+router.get('/id/:id', controller.getById);
+router.get('/nomeUsuario/:NomeUsuario', controller.getByNomeUsuario);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
